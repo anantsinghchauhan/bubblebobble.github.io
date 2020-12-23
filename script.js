b8b2a7cb62dba8ffa6e2914183e8b67ef987a481
anantsinghchauhan <57144246+anantsinghchauhan@users.noreply.github.com>
2020-12-23 16:50:24 +0530

    updated

---
+++ script.js
@@ -0,0 +1,47 @@
+var timer = 10;
+var score = 0;
+var timerelem = document.querySelector('#timerelem');
+var hitelem = document.querySelector('#hitelem');
+var btmpnl = document.querySelector('#btmpnl');
+var scoreelem = document.querySelector('#scoreelem');
+
+var guessNumber;
+
+
+setInterval(function(){
+    if(timer >0){
+        --timer;
+        timerelem.textContent = timer;
+    }
+    else{
+        btmpnl.innerHTML = '<h2 id="center">Game Over</h2>'
+    }
+}, 1000);
+
+
+function makeBubbles() {
+    for(var i =0; i<119; i++){
+        var randomNumber = Math.floor(Math.random()*10);
+        var template = `<div class="bubble">${randomNumber}</div>`;
+        btmpnl.innerHTML += template;
+    }
+}
+
+function getNewGuess(){
+    guessNumber = Math.floor(Math.random()*10);
+    hitelem.textContent = guessNumber;
+}
+
+btmpnl.addEventListener('click',function(details){
+    var clickedBblValue = details.target.textContent;
+    if(clickedBblValue == guessNumber){
+        score = score + 10;
+        scoreelem.textContent = score;
+        getNewGuess();
+        btmpnl.innerHTML = '';
+        makeBubbles();
+        }
+})
+
+getNewGuess();
+makeBubbles();
